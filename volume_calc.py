9bb084118041c2aae5e8454014459420c23fbcf4
alltimefreshman <163530613+alltimefreshman@users.noreply.github.com>
2024-03-15 22:36:09 +0900

    부피 계산 프로그램
    사용자에게 가로, 세로, 높이를 입력 받아 박스의 부피를 계산하는 프로그램

--- volume_calc.py
+++ volume_calc.py
@@ -1,12 +1,11 @@
-length = input("가로:12.5")
-width  = input("세로:30.2")
-height = input("높이:71")
+length = 12
+width  = 30
+height = 71
 
-length = float(12.5)
-width  = float(30.2)
-heigth = float(71)
+length = float("12.5")
+width  = float("30.2")
+heigth = float("71.5")
 
 volume = length * width * height
 
 print("박스의 부피는", volume, "입니다.")
-
