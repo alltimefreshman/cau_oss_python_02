1f92329728c3d7cf1a864429f8f392523e01665f
alltimefreshman <163530613+alltimefreshman@users.noreply.github.com>
2024-03-15 23:02:10 +0900

    부피 계산 프로그램
    사용자가 부피 계산을 용이하게 하도록 돕는 프로그램

--- volume_calc.py
+++ volume_calc.py
@@ -1,11 +1,11 @@
 length = 12
 width  = 30
-height = 71
+heigth = 71
 
 length = float("12.5")
 width  = float("30.2")
 heigth = float("71.5")
 
-volume = length * width * height
+volume = length * width * heigth
 
 print("박스의 부피는", volume, "입니다.")
